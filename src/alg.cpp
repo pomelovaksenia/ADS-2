424f0537661f96764113c49169e39d8dda884d17
pomelovaksenia <116708935+pomelovaksenia@users.noreply.github.com>
2023-03-12 21:01:50 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,25 +4,51 @@
 
 
 double pown(double value, uint16_t n) {
-
+  double res = 1;
+        while (n >= 1) {
+            res *= value;
+            n--;
+        }
+        return res;
 }
 
 uint64_t fact(uint16_t n) {
-
+  if (n == 0) {
+            return 1;
+        }
+        int res = 1;
+        while (n >= 1){
+            res *= n;
+            n--;
+        }
+        return res;
 }
 
 double calcItem(double x, uint16_t n) {
-
+  return (pown(x, n) / (fact(n)));
 }
 
 double expn(double x, uint16_t count) {
-
+  double res = 1;
+        while (count >= 1) {
+            res += calcItem(x, count);
+            count--;
+        }
+        return res;
 }
 
 double sinn(double x, uint16_t count) {
-
+  double res = x;
+        while (count >= 1) {
+            res += calcItem(x, (2 * count - 2)) * pown(-1, (count - 1));
+        }
+        return res;
 }
 
 double cosn(double x, uint16_t count) {
-
+  double res = 1;
+        while (count > 1) {
+            res += calcItem(x, (2 * count - 2)) * pown(-1, (count - 1));
+        }
+        return res;
 }
